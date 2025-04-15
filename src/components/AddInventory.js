d2e19737dc0266d30a3fc3d137716238981062bd
greeshma2309reddy <110275309+greeshma2309reddy@users.noreply.github.com>
2025-04-15 10:57:43 -0500

    Update AddInventory.js

--- src/components/AddInventory.js
+++ src/components/AddInventory.js
@@ -1,18 +1,1 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
-const AddInventory = () => {
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    name: '',
-    category: '',
-    sku: '',
-    quantity: '',
-    unit_price: '',
-    image_url: '',
-  });
-
-  return <div>Add Inventory</div>;
-};
-
-export default AddInventory;
