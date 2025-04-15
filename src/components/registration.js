0457dc8764f5f17cefa5f30aba215594e6119c8f
Praneeth171 <praneeth1117222@gmail.com>
2025-04-15 13:05:03 -0500

    Create registration.js

---
+++ src/components/registration.js
@@ -0,0 +1,9 @@
+// Change state initialization for better readability
+const [formData, setFormData] = useState({
+  name: '',
+  email: '',
+  id: '',
+  dept: '',
+  password: '',
+  role: 'user',
+});
