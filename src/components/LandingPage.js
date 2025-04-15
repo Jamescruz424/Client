e9d5c819aa6de044c01dded132c890621548fa00
Yash4054 <163705548+Yash4054@users.noreply.github.com>
2025-04-15 10:19:32 -0500

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 <div className="flex items-center">
   <Link to="/login">
     <button className="bg-white border border-blue-600 text-blue-600 px-4 py-2 text-sm font-medium mr-3">Sign In</button>
