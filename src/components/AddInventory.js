c3ca98bc72b6e6b8dc29225bf87a96285ec4d3b4
greeshma2309reddy <110275309+greeshma2309reddy@users.noreply.github.com>
2025-04-15 10:59:35 -0500

    Update AddInventory.js

--- src/components/AddInventory.js
+++ src/components/AddInventory.js
@@ -22,4 +22,10 @@ const AddInventory = () => {
 };
 
 export default AddInventory;
+<div className="bg-white p-6 rounded-lg shadow-sm border border-gray-200 max-w-lg mt-6">
+  <form>
+    {/* Form Fields will go here */}
+  </form>
+</div>
+
 
